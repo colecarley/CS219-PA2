a43b8b209aaaea7ddaaaca6ee325fe5ef6f6cf0d
colecarley <cole.carley@gmail.com>
2023-03-30 22:34:21 -0700

    flags working

--- arm.cpp
+++ arm.cpp
@@ -10,8 +10,8 @@ int main()
 {
     map<string, uint32_t (*)(uint32_t, uint32_t)> OPERATORS;
     OPERATORS["ADD"] = ADD;
-    OPERATORS["SUB"] = SUB;
     OPERATORS["AND"] = AND;
+    OPERATORS["SUB"] = SUB;
     OPERATORS["ASR"] = ASR;
     OPERATORS["LSL"] = LSL;
     OPERATORS["LSR"] = LSR;
@@ -31,13 +31,16 @@ int main()
         {
             reader >> hex >> operand2;
         }
+
         uint32_t output = OPERATORS[operation.substr(0, 3)](operand1, operand2);
         int32_t signCheck = output;
 
         string Z = output == 0 && operation.length() == 4 ? "Z: 1" : "Z: 0";
         string N = signCheck < 0x0 && operation.length() == 4 ? "\tN: 1" : "\tN: 0";
 
-        cout << operation << "\t" << operand1 << "\t" << operand2 << ": " << hex << "0x" << output << endl;
+        cout << operation << "\t"
+             << "0x" << hex << operand1 << "\t 0x" << operand2 << ": "
+             << "0x" << output << endl;
         cout << Z << N << endl;
     }
     return 0;
